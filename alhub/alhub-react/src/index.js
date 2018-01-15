22c798440dac5df37d1829c122ead0f53d9ae1d0
Mindaugas Urbontaitis <mindaugas@urbontatis.lt>
2018-01-15 18:26:08 +0200

    simple i18n

--- alhub/alhub-react/src/index.js
+++ alhub/alhub-react/src/index.js
@@ -5,12 +5,19 @@ import "bootstrap/dist/css/bootstrap.css";
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
+import { addLocaleData } from "react-intl";
+import en from "react-intl/locale-data/en";
+import lt from "react-intl/locale-data/lt";
 import { composeWithDevTools } from "redux-devtools-extension";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import rootReducer from "./rootReducer";
 import { userFetched, fetchCurrentUser } from "./actions/users";
 import setAuthorizationHeader from "./utils/setAuthorizationHeader";
+import { localetSet } from "./actions/locale";
+
+addLocaleData(en);
+addLocaleData(lt);
 
 const store = createStore(
   rootReducer,
@@ -22,6 +29,10 @@ if (localStorage.bookwormJWT) {
   store.dispatch(fetchCurrentUser());
 } else {
   store.dispatch(userFetched({}));
+}
+
+if (localStorage.alhubLang) {
+  store.dispatch(localetSet(localStorage.alhubLang));
 }
 
 ReactDOM.render(
---
+++ alhub/alhub-react/src/messages.js
@@ -0,0 +1,8 @@
+export default {
+  en: {
+    "nav.dashboard": "Dashboard"
+  },
+  lt: {
+    "nav.dashboard": "Apžvalgos langas"
+  }
+};
