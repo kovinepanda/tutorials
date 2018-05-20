a0a30bdd53ac3903037e7b3dc115d37c0dad95e4
Mindaugas Urbontaitis <mindaugas@urbontaitis.lt>
2018-05-20 13:13:59 +0300

    Communication between thread show and post editor

--- vueschool-forum/src/pages/PageThreadShow.vue
+++ vueschool-forum/src/pages/PageThreadShow.vue
@@ -2,30 +2,20 @@
   <div class="col-large push-top">
     <h1>{{thread.title}}</h1>
     <PostList :posts="posts"/>
-    <form @submit.prevent="addPost">
-      <div class="form-group">
-        <textarea
-          name=""
-          id=""
-          cols="30"
-          rows="10"
-          class="form-input"
-          v-model="newPostText"
-        ></textarea>
-      </div>
-      <div class="form-actions">
-        <button class="btn-blue">Submit post</button>
-      </div>
-    </form>
+    <PostEditor @save-post="addPost" :threadId="id" />
+    
   </div>
 </template>
 
 <script>
 import sourceData from '@/data'
 import PostList from '@/components/PostList'
+import PostEditor from '@/components/PostEditor'
+
 export default {
   components: {
-    PostList
+    PostList,
+    PostEditor
   },
   props: {
     id: {
@@ -35,8 +25,7 @@ export default {
   },
   data() {
     return {
-      thread: sourceData.threads[this.id],
-      newPostText: ''
+      thread: sourceData.threads[this.id]
     }
   },
   computed: {
@@ -48,19 +37,13 @@ export default {
     }
   },
   methods: {
-    addPost() {
-      const postId = 'greatPost' + Math.random()
-      const post = {
-        text: this.newPostText,
-        publishedAt: Math.floor(Date.now() / 1000),
-        threadId: this.id,
-        userId: 'jUjmgCurRRdzayqbRMO7aTG9X1G2',
-        '.key': postId
-      }
+    addPost(eventData) {
+      const post = eventData.post
+      const postId = eventData.post['.key']
+
       this.$set(sourceData.posts, postId, post)
       this.$set(this.thread.posts, postId, postId)
       this.$set(sourceData.users[post.userId].posts, postId, postId)
-      this.newPostText = ''
     }
   }
 }
