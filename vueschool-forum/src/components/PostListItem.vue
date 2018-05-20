12eba76b3474aac583cecabbb36269a4f863ce42
Mindaugas Urbontaitis <mindaugas@urbontaitis.lt>
2018-05-20 12:54:26 +0300

    Reply to the thread

--- vueschool-forum/src/components/PostListItem.vue
+++ vueschool-forum/src/components/PostListItem.vue
@@ -1,26 +1,25 @@
 <template>
-    <div class="post">
-        <div class="user-info">
-            <a href="#" class="user-name">{{user.name}}</a>
+  <div class="post">
+    <div class="user-info">
+      <a href="#" class="user-name">{{user.name}}</a>
 
-            <a href="profile.html#profile-details">
-                <img class="avatar-large" :src="user.avatar" alt="">
-            </a>
+      <a href="#">
+        <img class="avatar-large" :src="user.avatar" alt="">
+      </a>
 
-            <p class="desktop-only text-small">{{userPostsCount}} posts</p>
-        </div> 
-
-        <div class="post-content">
-            <div>            
-                {{post.text}}            
-            </div>        
-        </div>
-
-        <div class="post-date text-faded">
-            {{post.publishedAt}}
-        </div>
-
+      <p class="desktop-only text-small">{{userPostsCount}} posts</p>
     </div>
+
+    <div class="post-content">
+      <div>
+        {{post.text}}
+      </div>
+    </div>
+
+    <div class="post-date text-faded">
+      {{post.publishedAt}}
+    </div>
+  </div>
 </template>
 
 <script>
--- vueschool-forum/src/pages/PageThreadShow.vue
+++ vueschool-forum/src/pages/PageThreadShow.vue
@@ -1,16 +1,28 @@
 <template>
- <div>    
-    <div class="col-large push-top">
-      <h1>{{thread.title}}</h1>      
-      <PostList :posts="posts" />
-    </div>
+  <div class="col-large push-top">
+    <h1>{{thread.title}}</h1>
+    <PostList :posts="posts"/>
+    <form @submit.prevent="addPost">
+      <div class="form-group">
+        <textarea
+          name=""
+          id=""
+          cols="30"
+          rows="10"
+          class="form-input"
+          v-model="newPostText"
+        ></textarea>
+      </div>
+      <div class="form-actions">
+        <button class="btn-blue">Submit post</button>
+      </div>
+    </form>
   </div>
 </template>
 
 <script>
 import sourceData from '@/data'
 import PostList from '@/components/PostList'
-
 export default {
   components: {
     PostList
@@ -23,7 +35,8 @@ export default {
   },
   data() {
     return {
-      thread: sourceData.threads[this.id]
+      thread: sourceData.threads[this.id],
+      newPostText: ''
     }
   },
   computed: {
@@ -32,6 +45,22 @@ export default {
       return Object.values(sourceData.posts).filter(post =>
         postIds.includes(post['.key'])
       )
+    }
+  },
+  methods: {
+    addPost() {
+      const postId = 'greatPost' + Math.random()
+      const post = {
+        text: this.newPostText,
+        publishedAt: Math.floor(Date.now() / 1000),
+        threadId: this.id,
+        userId: 'jUjmgCurRRdzayqbRMO7aTG9X1G2',
+        '.key': postId
+      }
+      this.$set(sourceData.posts, postId, post)
+      this.$set(this.thread.posts, postId, postId)
+      this.$set(sourceData.users[post.userId].posts, postId, postId)
+      this.newPostText = ''
     }
   }
 }
