67c86dc0be9bb16240c5990409de345cbb0355cf
Mindaugas Urbontaitis <mindaugas@urbontaitis.lt>
2018-05-20 10:18:10 +0300

    import forum global css style

--- vueschool-forum/src/components/HelloWorld.vue
+++ vueschool-forum/src/components/HelloWorld.vue
@@ -1,14 +1,29 @@
 <template>
-  <div>
-    <h1>Awesome forum </h1>
-    <div v-for="thread in threads">
-      <h2>{{thread.title}}</h2>
-      <div v-for="postId in thread.posts">
-        <p>{{users[posts[postId].userId].name}}</p>
-        <p>{{posts[postId].text}}</p>
+  <div>    
+    <div v-for="thread in threads" class="col-large push-top">
+      <h1>{{thread.title}}</h1>      
+      <div class="post-list">  
+        <div v-for="postId in thread.posts" class="post">
+          <div class="user-info">
+            <a href="#" class="user-name">{{users[posts[postId].userId].name}}</a>
+
+            <a href="#">
+              <img class="avatar-large" :src="users[posts[postId].userId].avatar" alt="">
+            </a>
+
+            <p class="desktop-only text-small">107 posts</p>
+          </div>
+          <div class="post-content">
+            <div>
+              {{posts[postId].text}}
+            </div>
+          </div>
+          <div class="post-date text-faded">
+            {{posts[postId].publishedAt}}
+            </div>
+        </div>      
       </div>
     </div>
-    
   </div>
 </template>
 
@@ -29,23 +44,3 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1,
-h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
-</style>
