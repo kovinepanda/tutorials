a523899f7fc3077bce07ea472a578cfda275a88d
Mindaugas Urbontaitis <mindaugas@urbontaitis.lt>
2018-05-20 11:35:53 +0300

    page not found component

---
+++ vueschool-forum/src/pages/PageNotFound.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="col-full">
+        <h1>Not Found</h1>
+        <p>Go 
+            <router-link :to="{name: 'Home'}">
+                Home
+            </router-link>
+        </p>
+    </div>
+</template>
+
+<style scoped>
+h1 {
+  font-size: 100px;
+}
+
+p {
+  font-size: 50px;
+  font-weight: 100;
+}
+
+div {
+  text-align: center;
+}
+</style>
