6b189dfd7a49f63970b684e5d026b0f4120d456e
Mindaugas Urbontaitis <mindaugas@urbontaitis.lt>
2018-09-10 23:11:25 +0300

    Init firebase

--- vue-tutorials/forum/config/dev.env.js
+++ vue-tutorials/forum/config/dev.env.js
@@ -1,7 +1,8 @@
 'use strict'
 const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
+const firebaseEnv = require('./firebase.env')
 
-module.exports = merge(prodEnv, {
+module.exports = merge(firebaseEnv, prodEnv, {
   NODE_ENV: '"development"'
 })
--- vue-tutorials/forum/src/main.js
+++ vue-tutorials/forum/src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import firebase from 'firebase'
 import App from './App'
 import router from './router'
 import store from '@/store'
@@ -9,6 +10,17 @@ import AppDate from '@/components/AppDate'
 Vue.component('AppDate', AppDate)
 
 Vue.config.productionTip = false
+
+const config = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DB_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MSG_SENDER_ID
+}
+console.log(config)
+firebase.initializeApp(config)
 
 /* eslint-disable no-new */
 new Vue({
