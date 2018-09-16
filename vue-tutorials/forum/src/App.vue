46712d7096082bf966df167166ef834bf4ee638b
Mindaugas Urbontaitis <mindaugas@urbontaitis.lt>
2018-09-16 19:52:41 +0300

    Show a progress bar while components fetch async data

--- vue-tutorials/forum/src/App.vue
+++ vue-tutorials/forum/src/App.vue
@@ -2,13 +2,14 @@
   <div id="app">
     <TheNavBar />
     <div class="container">
-      <router-view v-show="showPage" @ready="showPage = true"/>
+      <router-view v-show="showPage" @ready="pageReady"/>
       <AppSpinner v-show="!showPage" />
     </div>
   </div>
 </template>
 
 <script>
+import NProgress from 'nprogress'
 import AppSpinner from '@/components/AppSpinner'
 import TheNavBar from '@/components/TheNavBar'
 
@@ -24,9 +25,22 @@ export default {
     }
   },
 
+  methods: {
+    pageReady() {
+      this.showPage = true
+      NProgress.done()
+    }
+  },
+
   created() {
+    NProgress.configure({
+      speed: 200,
+      showSpinner: true
+    })
+    NProgress.start()
     this.$router.beforeEach((to, from, next) => {
       this.showPage = false
+      NProgress.start()
       next()
     })
   }
@@ -35,4 +49,9 @@ export default {
 
 <style>
 @import 'assets/css/style.css';
+@import '~nprogress/nprogress.css';
+
+#nprogress .bar {
+  background: red;
+}
 </style>
