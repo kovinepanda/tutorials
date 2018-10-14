2b75fb44fce2f76c0c069656faaf5026705dec76
Mindaugas Urbontaitis <mindaugas@urbontaitis.lt>
2018-10-14 16:16:02 +0300

    Profile page of authenticated user

--- vue-tutorials/forum/src/pages/PageProfile.vue
+++ vue-tutorials/forum/src/pages/PageProfile.vue
@@ -30,6 +30,7 @@ import PostList from '@/components/PostList'
 import UserProfileCard from '@/components/UserProfileCard'
 import UserProfileCardEditor from '@/components/UserProfileCardEditor'
 import { mapGetters } from 'vuex'
+import asyncDataStatus from '@/mixins/asyncDataStatus'
 
 export default {
   components: {
@@ -37,6 +38,8 @@ export default {
     UserProfileCard,
     UserProfileCardEditor
   },
+
+  mixins: [asyncDataStatus],
 
   props: {
     edit: {
@@ -51,18 +54,14 @@ export default {
     }),
 
     userPosts() {
-      if (this.user.posts) {
-        return Object.values(this.$store.state.posts).filter(
-          post => post.userId === this.user['.key']
-        )
-      }
-
-      return []
+      return this.$store.getters.userPosts(this.user['.key'])
     }
   },
 
   created() {
-    this.$emit('ready')
+    this.$store
+      .dispatch('fetchPosts', { ids: this.user.posts })
+      .then(() => this.asyncDataStatus_fetched())
   }
 }
 </script>
